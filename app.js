9bd94557b4267fcf02ffde347d0e6359c6e024d6
almialmi <almialmi61621@gmail.com>
2021-12-14 09:38:05 +0300

    add some change

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const limiter = rateLimit({
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({limit: '50mb',extended:true}));
 app.use(cors({
-    origin:['http://localhost:3000'],
+    origin:['http://localhost:3000','https://ecommerceclientweb.herokuapp.com/'],
     credentials: true
   }));
 app.use('/api/productImages',express.static('productImages'))
