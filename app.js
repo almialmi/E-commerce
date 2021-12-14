87efd31dc825decb0f48a1c599f2ad974bd94d00
almialmi <almialmi61621@gmail.com>
2021-12-14 13:58:29 +0300

    flutter images add

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ app.use(function (req, res, next) {
   next();
 });
 app.use('/api/productImages', express.static('productImages'))
+app.use('/api/flutterImages', express.static('flutterImages'))
 app.use(cookieParser());
 app.use(mongoSanitize());
 app.use(helmet());
